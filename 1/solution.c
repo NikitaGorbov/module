08da40e35decb1b8fe878e5bb4bb7bb09c87b15d
Nikita Gorbov <root@homer>
2022-07-04 06:01:09 +0000

    cleanup code

--- 1/solution.c
+++ 1/solution.c
@@ -3,7 +3,6 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Nikita Gorbov <nikita.gorbov.2001@gmail.com>");
-MODULE_DESCRIPTION("Char device driver");
 MODULE_VERSION("0.01");
 
 static int device_open(struct inode *, struct file *);
@@ -11,12 +10,6 @@ static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char *, size_t, loff_t *);
 static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
-static struct file_operations file_ops = {
-	.read = device_read,
-	.write = device_write,
-	.open = device_open,
-	.release = device_release
-};
 static int __init solution_init(void)
 {
 	return 0;
