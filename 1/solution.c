940f94e7d62c9f67a2ceebceb792d95879653bc5
Nikita Gorbov <nikita.gorbov.2001@gmail.com>
2022-07-05 11:08:12 +0600

    finish task 3

--- 1/solution.c
+++ 1/solution.c
@@ -5,13 +5,9 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Nikita Gorbov <nikita.gorbov.2001@gmail.com>");
 MODULE_VERSION("0.01");
 
-static int device_open(struct inode *, struct file *);
-static int device_release(struct inode *, struct file *);
-static ssize_t device_read(struct file *, char *, size_t, loff_t *);
-static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
-
 static int __init solution_init(void)
 {
+	printk(KERN_INFO "test \nkernel_mooc\n");
 	return 0;
 }
 
--- 3/solution.c
+++ 3/solution.c
@@ -1,7 +1,7 @@
-/* WORK IN PROGRESS */
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/random.h>
+#include "checker.h"
 
 #define MAX_ARR_SIZE 10
 
@@ -10,35 +10,58 @@ MODULE_AUTHOR("Nikita Gorbov <nikita.gorbov.2001@gmail.com>");
 MODULE_VERSION("0.01");
 
 /* Return array size */
-static size_t fill_array(short *arr) {
-	size_t size;
+size_t fill_array(short *arr)
+{
+    size_t size;
 
-	get_random_bytes(&size, sizeof(int));
-	if (size < 0)
-		size = -size;
-	size %= MAX_ARR_SIZE;
-	if (!size) 
-		size = MAX_ARR_SIZE;
-	printk(KERN_ERR "size = %zu\n", size);
-	get_random_bytes(arr, sizeof(short) * size);
-	return size;
+    get_random_bytes(&size, sizeof(int));
+    if (size < 0)
+        size = -size;
+    size %= MAX_ARR_SIZE;
+    if (!size) 
+        size = MAX_ARR_SIZE;
+    get_random_bytes(arr, sizeof(short) * size);
+    return size;
 }
 
-static int __init solution_init(void)
+int my_array_sum(short *array, size_t n)
 {
-	int i;
-	size_t arr_size;
-	short array[MAX_ARR_SIZE];
+    int i, sum = 0;
 
-	arr_size = fill_array(array);
-	for (i = 0; i < arr_size; i++) {
-		printk(KERN_INFO "%d\n", array[i]);
-	}
-	return 0;
+    for (i = 0; i < n; i++) {
+        sum += array[i];
+    }
+    return sum;
 }
 
-static void __exit solution_exit(void)
+int __init solution_init(void)
 {
+    CHECKER_MACRO;
+    int i, j, result;
+    size_t arr_size;
+    short array[MAX_ARR_SIZE];
+    char buf[256];
+    ssize_t buf_size;
+
+    for (j = 0; j < 10; j++) {
+        arr_size = fill_array(array);
+        for (i = 0; i < arr_size; i++) {
+            printk(KERN_INFO "%d\n", array[i]);
+        }
+        result = array_sum(array, arr_size);
+        buf_size = generate_output(result, array, arr_size, buf);
+        if (result == my_array_sum(array, arr_size)) {
+            printk(KERN_INFO "%s", buf);
+        } else {
+            printk(KERN_ERR "%s", buf);
+        }
+    }
+    return 0;
+}
+
+void __exit solution_exit(void)
+{
+    CHECKER_MACRO;
 }
 
 module_init(solution_init);
